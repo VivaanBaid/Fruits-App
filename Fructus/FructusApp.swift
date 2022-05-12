c2183bdc94e199a356e231cfa2387d12d5519bbf
Vivaan Baid <baidvivaan782@gmail.com>
2022-05-12 07:20:02 -0400

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Vivaan Baid on 12/05/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
